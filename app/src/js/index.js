118fc6d9ef8d3571cd904f2bdd2d6dbe0fb5ff4a
DESKTOP-MDCVNIO\Adam <wengjin456123@gmail.com>
2017-07-02 16:36:26 +0800

    completed

---
+++ app/src/js/index.js
@@ -0,0 +1,4 @@
+require('../index.html')
+require('../scss/style.css')
+require('../scss/test.scss')
+document.write('My name is Adam hehehee');
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack')
+var path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var _entry = {
+    "index": './app/src/js/index.js',
+};
+module.exports = {
+  entry: _entry,
+  output: {
+    path: path.join(__dirname, 'app','dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ExtractTextPlugin.extract({
+          use:[ 'css-loader','sass-loader'],
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.html$/,
+        loader: "raw-loader" // loaders: ['raw-loader'] is also perfectly acceptable.
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin('This file is created by Adam'),
+    new ExtractTextPlugin({
+      filename: 'index.css',
+      disable: false,
+      allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './app/src/index.html'
+    })
+  ]
+}
